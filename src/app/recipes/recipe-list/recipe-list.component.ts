4e2032e57545fc32280f6676a92083c58bb8328d
Altay Karakuş <altaykarakus@gmail.com>
2020-07-21 02:19:20 +0300

    added recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.module';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+      'This is a recipe',
+      'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg'
+    ),
+    new Recipe('Krep',
+    'This is a recipe',
+    'https://cdn.pixabay.com/photo/2017/09/30/12/12/pancakes-2801959_1280.jpg'
+  )
+  ];
 
   constructor() { }
 
